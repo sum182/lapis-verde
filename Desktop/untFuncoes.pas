9248d209cfd1a44b2edec69dd768603efa5efd9a
Leonardo Lima da Silva <leolima80rj@gmail.com>
2016-04-06 01:39:36 -0300

    Bloqueio de exclusão de alunos e turmas quando estes tiverem Agenda(s) associada(s).

--- Desktop/untFuncoes.pas
+++ Desktop/untFuncoes.pas
@@ -6,7 +6,7 @@ Uses
 
   Classes,  DBClient,  DB, SqlExpr,FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param, FireDAC.Stan.Error,
   FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, FireDAC.Comp.DataSet,
-  FireDAC.Comp.Client,smCadPadrao;
+  FireDAC.Comp.Client,smCadPadrao, System.SysUtils;
 
   procedure SetIdEscolaCadastro(Dataset: TDataset);
   procedure SetSQLEscolaIdBusca(smCadPadrao: TsmCadPadrao);
@@ -20,6 +20,9 @@ Uses
   function GetNomeAplicacao:string;
   function GetEscolaId:integer;
   function UsuarioIsAdminSistema:boolean;
+
+  function CheckAgendaAluno(idAluno: Integer): boolean;
+  function CheckAgendaTurma(idTurma: Integer): boolean;
 
 implementation
 
@@ -84,5 +87,48 @@ function UsuarioIsAdminSistema:boolean;
 begin
    Result:= DM.fUsuarioAdminSistema;
 end;
+
+function CheckAgendaAluno(idAluno: Integer): boolean;
+var quCheckAgenda: TFDQuery;
+    sSQL: string;
+begin
+  sSQL := 'select count(1) as Qtde ' +
+            'from agendadb.agenda_aluno aa ' +
+                 'inner join agendadb.agenda a on (a.agenda_id = aa.agenda_id) ' +
+           'where (aa.aluno_id = ' + IntToStr(idAluno) + ') ' +
+             'and (a.escola_id = ' + IntToStr(GetEscolaId) + ')';
+
+  quCheckAgenda:= TFDQuery.Create(nil);
+  quCheckAgenda.Close;
+  quCheckAgenda.Connection := DM.FDConnection;
+  quCheckAgenda.SQL.Add(sSQL);
+  quCheckAgenda.Open();
+  Result := quCheckAgenda.FieldByName('Qtde').AsInteger > 0;
+  quCheckAgenda.Close;
+  FreeAndNil(quCheckAgenda);
+end;
+
+
+function CheckAgendaTurma(idTurma: Integer): boolean;
+var quCheckAgenda: TFDQuery;
+    sSQL: string;
+begin
+  sSQL := 'select count(1) as Qtde ' +
+            'from agendadb.agenda_turma au ' +
+                 'inner join agendadb.agenda a on (a.agenda_id = au.agenda_id) ' +
+           'where (au.turma_id = ' + IntToStr(idTurma) + ') ' +
+             'and (a.escola_id = ' + IntToStr(GetEscolaId) + ')';
+
+  quCheckAgenda := TFDQuery.Create(nil);
+  quCheckAgenda.Close;
+  quCheckAgenda.Connection := DM.FDConnection;
+  quCheckAgenda.SQL.Clear;
+  quCheckAgenda.SQL.Add(sSQL);
+  quCheckAgenda.Open();
+  Result := quCheckAgenda.FieldByName('Qtde').AsInteger > 0;
+  FreeAndNil(quCheckAgenda);
+end;
+
+
 
 end.
